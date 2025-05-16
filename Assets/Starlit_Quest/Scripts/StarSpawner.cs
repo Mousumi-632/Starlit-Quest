e40e80bdc1b229da195052aba6de1527c59ede2b
Mousumi-632 <mousumi.mukherjee@hyperisland.se>
2025-05-17 01:23:32 +0200

    Added Target Star spawner

---
+++ Assets/Starlit_Quest/Scripts/StarSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class StarSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject starPrefab;
+    [SerializeField] private Transform cloudTransform;
+    [SerializeField] private float behindDistance = 1f;
+    [SerializeField] private float starHeight = 5f;
+
+    void Start()
+    {
+        SpawnStarBehindCloud();
+    }
+
+    void SpawnStarBehindCloud()
+    {
+        // 🧠 Use the opposite of cloud's forward direction to find "behind"
+        Vector3 spawnPosition = cloudTransform.position - cloudTransform.forward * behindDistance;
+
+        // Match the height to the cloud
+        spawnPosition.y = starHeight;
+
+        // Spawn the star at the calculated position
+        Instantiate(starPrefab, spawnPosition, Quaternion.identity);
+    }
+}
