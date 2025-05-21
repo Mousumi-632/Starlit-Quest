646be8f609c03e8d7ba7a0c9100f15ef673a2a9c
ignacioRoulet <ignacio.roulet@hyperisland.se>
2025-05-21 11:35:34 +0200

    P_GameManager Optimized

--- Assets/Starlit_Quest/Scripts/GameManager.cs
+++ Assets/Starlit_Quest/Scripts/GameManager.cs
@@ -2,30 +2,45 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject cloudObject;           
-    [SerializeField] private GameObject cometSpawnerObject;   
-    [SerializeField] private GameObject starSpawnerObject;    
+    [SerializeField] private GameObject cloudPrefab;
+    [SerializeField] private GameObject cometSpawnerPrefab;
+    [SerializeField] private GameObject starSpawnerPrefab;
+    [SerializeField] private GameObject XROriginPrefab;
+    [SerializeField] private GameObject starCounterPrefab;
 
-    private void Start()
+    private void Awake()
     {
-        if (starSpawnerObject != null)
-            starSpawnerObject.SetActive(true);
+        DontDestroyOnLoad(gameObject);
+        Instantiate(XROriginPrefab);   
+    }
+
+        private void Start()
+    {
+        if (starCounterPrefab != null)
+            Instantiate(starCounterPrefab);
+        if (starSpawnerPrefab != null)
+            Instantiate(starSpawnerPrefab);
 
         StarCounter.Instance.OnStarsChanged += OnStarCollected;
-
     }
 
     private void OnStarCollected(int count)
     {
         if (count == 1)
         {
-            if (cloudObject != null)
-                cloudObject.SetActive(true);
+            if (cloudPrefab != null)
+                Instantiate(cloudPrefab);
+
+            if (starSpawnerPrefab != null)
+                Instantiate(starSpawnerPrefab);
         }
         else if (count == 2)
         {
-            if (cometSpawnerObject != null)
-                cometSpawnerObject.SetActive(true);
+            if (cometSpawnerPrefab != null)
+                Instantiate(cometSpawnerPrefab);
+
+            if (starSpawnerPrefab != null)
+                Instantiate(starSpawnerPrefab);
         }
         else if (count == 3)
         {
