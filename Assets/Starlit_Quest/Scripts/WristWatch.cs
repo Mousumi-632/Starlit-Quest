ed8b7f3dd1ddb170b7ff9ae54d83c9f69b757c6a
Jiayao Yu <yjybxx2@yahoo.com>
2025-05-21 11:46:26 +0200

    Added wrist watch script. To be tested.

--- Assets/Starlit_Quest/Scripts/WristWatch.cs
+++ Assets/Starlit_Quest/Scripts/WristWatch.cs
@@ -1,10 +1,30 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WristWatch : MonoBehaviour
 {
-    [Header("Target Star Reference")]
-    [SerializeField] private RawImage targetStarImage;
-    
+    [Header("Target Star Display")] [SerializeField]
+    private RawImage targetStarImage;
 
+    [Header("Target Stars")] [SerializeField]
+    private List<Texture> targetStars;
+
+    private void Start()
+    {
+        StarCounter.Instance.OnStarsChanged += UpdateWatchStarDisplay;
+    }
+
+    private void UpdateWatchStarDisplay(int countCollectedStars)
+    {
+        targetStarImage.texture = targetStars[countCollectedStars];
+    }
 }
+
+
+// TODOs
+//     call StarCounter AddStar()
+//     verify raw image texture switch
+//     add Game Manager game object, verify the rest
+//     
