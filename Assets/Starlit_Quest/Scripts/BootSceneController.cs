ec695bbaf48656e9f5b87cce2b6e3d8055295eae
ignacioRoulet <ignacio.roulet@hyperisland.se>
2025-05-23 00:39:59 +0200

    SceneController added /Transition scene logic

---
+++ Assets/Starlit_Quest/Scripts/BootSceneController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BootSceneController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void Initialize(GameManager gameManager)
+    {
+        // 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
