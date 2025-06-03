610badbc111754b989d92b28b8db49a28a3c82c4
ignacioRoulet <ignacio.roulet@hyperisland.se>
2025-06-03 12:05:41 +0200

    Spatial audio implemented / New clips

--- Assets/Starlit_Quest/Scripts/NPCSoundManager.cs
+++ Assets/Starlit_Quest/Scripts/NPCSoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -18,28 +19,57 @@ public class NPCSoundManager : MonoBehaviour
     private void Awake()
     {
         npcSource = GetComponent<AudioSource>();
-
         npcSource.outputAudioMixerGroup = npcGroup;
-        npcSource.spatialBlend = 1f;          
-        npcSource.minDistance = 1f;          
+        npcSource.spatialBlend = 1f;
+        npcSource.minDistance = 1f;
         npcSource.maxDistance = 15f;
         npcSource.rolloffMode = AudioRolloffMode.Linear;
         npcSource.playOnAwake = false;
         npcSource.loop = false;
     }
 
-    public void PlayNPCDialogue(int starsCollected, int maxStars)
+    private void Start()
     {
-        if (starsCollected == 0)
-            PlaySound(npcIntroClip);
-        else if (starsCollected == 1)
-            PlaySound(npcOneClip);
-        else if (starsCollected == 2)
-            PlaySound(npcTwoClip);
-        else if (starsCollected == maxStars)
-            PlaySound(npcThreeClip);
+        StartCoroutine(WaitForStarCounter());
     }
 
+    private IEnumerator WaitForStarCounter()
+    {
+        while (StarCounter.Instance == null)
+            yield return null;
+
+        // Subscribe to star updates
+        StarCounter.Instance.OnStarsChanged += OnStarsChanged;
+
+        // Play current state just in case stars were added before this subscribed
+        OnStarsChanged(StarCounter.Instance.StarsCollected);
+    }
+
+    private void OnDestroy()
+    {
+        if (StarCounter.Instance != null)
+            StarCounter.Instance.OnStarsChanged -= OnStarsChanged;
+    }
+
+    private void OnStarsChanged(int starsCollected)
+    {
+        int maxStars = StarCounter.Instance.MaxStars;
+        PlayNPCDialogue(starsCollected, maxStars);
+    }
+
+    public void PlayNPCDialogue(int starsCollected, int maxStars)
+    {
+        Debug.Log($"NPC Dialogue Triggered: Stars = {starsCollected}, Max = {maxStars}");
+
+        if (starsCollected == 0)
+            PlaySound(npcIntroClip);
+        else if (starsCollected == maxStars)
+            PlaySound(npcThreeClip);
+        else if (starsCollected == 2)
+            PlaySound(npcTwoClip);
+        else if (starsCollected == 1)
+            PlaySound(npcOneClip);
+    }
 
     public void PlaySound(AudioClip clip)
     {
