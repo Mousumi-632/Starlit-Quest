d62ff8a5146d3ade5b758766ee2f2ef8df66e32e
HyperTotte <totte.moork@hyperisland.se>
2025-06-09 01:17:08 +0200

    updated glow script

--- Assets/JarGlowController.cs
+++ Assets/JarGlowController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class JarGlowController : MonoBehaviour
 {
@@ -7,11 +8,12 @@ public class JarGlowController : MonoBehaviour
     [SerializeField] private Color glowColor = Color.yellow;
     [SerializeField] private float minGlow = 0.2f;
     [SerializeField] private float maxGlow = 5f;
+    [SerializeField] private float glowTransitionTime = 0.5f;
+    [SerializeField] private AnimationCurve glowCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
     private int currentStars = 0;
     private Material glowMaterial;
-
-
+    private Coroutine glowCoroutine;
 
     private void Start()
     {
@@ -19,7 +21,7 @@ public class JarGlowController : MonoBehaviour
             jarRenderer = GetComponent<Renderer>();
 
         glowMaterial = jarRenderer.materials[1];
-        UpdateGlow();
+        UpdateGlow(); // start with correct glow
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,14 +33,39 @@ public class JarGlowController : MonoBehaviour
         }
     }
 
-
     private void UpdateGlow()
     {
         float t = (float)currentStars / maxStars;
-        float intensity = Mathf.Lerp(minGlow, maxGlow, t);
-        Color finalColor = glowColor * intensity;
+        float targetIntensity = Mathf.Lerp(minGlow, maxGlow, glowCurve.Evaluate(t));
 
-        glowMaterial.SetColor("_EmissionColor", finalColor);
-        DynamicGI.SetEmissive(jarRenderer, finalColor);
+        if (glowCoroutine != null)
+            StopCoroutine(glowCoroutine);
+
+        glowCoroutine = StartCoroutine(SmoothGlowChange(targetIntensity));
+    }
+
+    private IEnumerator SmoothGlowChange(float targetIntensity)
+    {
+        Color currentColor = glowMaterial.GetColor("_EmissionColor");
+        float currentIntensity = currentColor.maxColorComponent;
+        float elapsed = 0f;
+
+        while (elapsed < glowTransitionTime)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / glowTransitionTime;
+            float newIntensity = Mathf.Lerp(currentIntensity, targetIntensity, t);
+            Color finalColor = glowColor * newIntensity;
+
+            glowMaterial.SetColor("_EmissionColor", finalColor);
+            DynamicGI.SetEmissive(jarRenderer, finalColor);
+
+            yield return null;
+        }
+
+        // Ensure final value is set
+        Color final = glowColor * targetIntensity;
+        glowMaterial.SetColor("_EmissionColor", final);
+        DynamicGI.SetEmissive(jarRenderer, final);
     }
 }
