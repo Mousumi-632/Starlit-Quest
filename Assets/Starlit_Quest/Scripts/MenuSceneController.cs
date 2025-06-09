574987840365298e9a21cfefe261e4031492d2c8
ignacioRoulet <ignacio.roulet@hyperisland.se>
2025-06-09 15:54:48 +0200

    MenuSceneController fixed w/ IntroStory

--- Assets/Starlit_Quest/Scripts/MenuSceneController.cs
+++ Assets/Starlit_Quest/Scripts/MenuSceneController.cs
@@ -1,12 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class MenuSceneController : MonoBehaviour
 {
     private GameManager gameManager;
 
+    [SerializeField] private GameObject startButtonPrefab;
+
+    private AudioSource audioSource;
+
     public void Initialize(GameManager gameManager)
     {
         this.gameManager = gameManager;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(ShowStartButtonAfterAudio());
+    }
+
+    private IEnumerator ShowStartButtonAfterAudio()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+
+        Instantiate(startButtonPrefab, transform);
     }
 
     public void LoadStarlitQuestScene()
